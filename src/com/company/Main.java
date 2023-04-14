187c78802a6c237d5dd8faa06c53e1fa217ae9a3
Owen Riske <owenr0129@5565.ca>
2023-04-14 09:20:23 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    colour red=new colour(255,0,0,"Red");
+
+
+red.displayColour();        System.out.println(red.getHexCodeEfficiently());
+
+
+
+    }
+
+}
---
+++ src/com/company/colour.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class colour {
+    int red;
+    int green;
+    int blue;
+    String commonName;
+
+    colour(int R, int G, int B, String commonName){
+        this.red=R;
+        this.green=G;
+        this.blue=B;
+        this.commonName=commonName;
+    }
+
+    int getRed(){
+        return red;
+    }
+
+    int getGreen(){
+        return green;
+    }
+
+    int getBlue(){
+        return blue;
+    }
+
+    int[] getRGB(){
+        return new int[] {red,green,blue};
+    }
+
+
+    /*
+    String getHexCode(){
+        String hexCode="#";
+
+        //add red to the end of the hex string
+        if(Integer.toHexString(red).length()<2){
+            hexCode+="0";
+        }
+        hexCode+=Integer.toHexString(red);
+
+        //add green to the end of the hex string
+        if(Integer.toHexString(green).length()<2){
+            hexCode+="0";
+        }
+        hexCode+=Integer.toHexString(green);
+
+        //add blue to the end of the hex string
+        if(Integer.toHexString(blue).length()<2){
+            hexCode+="0";
+        }
+        hexCode+=Integer.toHexString(blue);
+
+        return hexCode;
+    }
+
+     */
+
+    //removes the redundant code
+    String getHexCodeEfficiently(){
+        String hexCode="#";
+        for (int c:getRGB()) {
+            if(Integer.toHexString(c).length()<2){
+                hexCode+="0";
+            }
+            hexCode+=Integer.toHexString(c);
+        }
+        return hexCode;
+    }
+    void displayColour(){
+        System.out.println("\033[48;2;"+red+";"+green+";"+blue+"m");
+
+    }
+}
